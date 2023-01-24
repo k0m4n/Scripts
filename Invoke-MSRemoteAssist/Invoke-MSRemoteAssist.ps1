d66928ce7e7aaaec5ef7ce83c05bf642d37d5ff8
k0m4n <github@k0m4n.anonaddy.me>
2023-01-24 01:06:09 -0800

    Ad headder

--- Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
+++ Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
@@ -1,4 +1,8 @@
-﻿$quickassist = "$env:windir\system32\quickassist.exe"
+﻿# Invoke-MSRemoteAssist.ps1
+# Requres Windows 7 or newer 
+# Locates the newest Bulit-In Remote Asstance Tool and launches it in PIN mode
+
+$quickassist = "$env:windir\system32\quickassist.exe"
 $msra = "$env:windir\system32\msra.exe"
 If(Test-Path $quickassist)
 {
