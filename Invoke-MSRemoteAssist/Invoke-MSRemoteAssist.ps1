7ad984541f4f31fb3b6cb71c6719f8b91470d434
k0m4n <github@k0m4n.anonaddy.me>
2023-01-24 01:04:00 -0800

    Add MS Remore Assist

---
+++ Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
@@ -0,0 +1,10 @@
+﻿$quickassist = "$env:windir\system32\quickassist.exe"
+$msra = "$env:windir\system32\msra.exe"
+If(Test-Path $quickassist)
+{
+    Start-Process $quickassist
+}
+else
+{
+    Start-Process $msra -ArgumentList "/geteasyhelp"
+}
