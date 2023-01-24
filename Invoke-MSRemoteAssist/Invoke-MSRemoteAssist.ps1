d4140e62c6ef7e1eaa86c1b8dea8f09bcfbd86b7
k0m4n <github@k0m4n.anonaddy.me>
2023-01-24 12:05:25 -0800

    New Script desriiption based on chatGPT

--- Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
+++ Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
@@ -1,7 +1,12 @@
 ﻿# Invoke-MSRemoteAssist.ps1
 # Author : k0m4n
-# Requres Windows 7 or newer 
-# Locates the newest Bulit-In Remote Asstance Tool and launches it in PIN mode
+#
+# This script is used to launch the Remote Assistance tool on a Windows system. 
+# The script first checks if the Quick Assist app is installed by querying the AppxPackage for "MicrosoftCorporationII.QuickAssist". 
+# If it exists, it starts the app. If not, it checks for the presence of the "quickassist.exe" file in the system32 folder and starts it if it is present. 
+# If neither of those are found, it starts the "msra.exe" file with the argument "/geteasyhelp". 
+# The Remote Assistance tool allows a user to remotely connect to and control another user's computer to provide technical assistance.
+
 
 $APPXQassist = Get-AppxPackage MicrosoftCorporationII.QuickAssist
 $quickassist = "$env:windir\system32\quickassist.exe"
