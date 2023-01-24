337f5efaaa99f34e53847468b497b85d3ae59062
k0m4n <github@k0m4n.anonaddy.me>
2023-01-24 01:18:25 -0800

    Add APPX\Store Version Of QuickAssist

--- Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
+++ Invoke-MSRemoteAssist/Invoke-MSRemoteAssist.ps1
@@ -1,10 +1,16 @@
 ﻿# Invoke-MSRemoteAssist.ps1
+# Author : k0m4n
 # Requres Windows 7 or newer 
 # Locates the newest Bulit-In Remote Asstance Tool and launches it in PIN mode
 
+$APPXQassist = Get-AppxPackage MicrosoftCorporationII.QuickAssist
 $quickassist = "$env:windir\system32\quickassist.exe"
 $msra = "$env:windir\system32\msra.exe"
-If(Test-Path $quickassist)
+IF($APPXQassist)
+{
+    Start-Process shell:AppsFolder\MicrosoftCorporationII.QuickAssist_8wekyb3d8bbwe!App
+}
+ElseIf(Test-Path $quickassist)
 {
     Start-Process $quickassist
 }
