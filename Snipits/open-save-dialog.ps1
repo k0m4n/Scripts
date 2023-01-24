de70e41c16ef685675e6dae3986551a69a7579d5
k0m4n <github@k0m4n.anonaddy.me>
2023-01-24 03:21:50 -0800

    upload snipits

---
+++ Snipits/open-save-dialog.ps1
@@ -0,0 +1,39 @@
+﻿
+Function Get-OpenFile($initialDirectory)
+{ 
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+Out-Null
+
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.initialDirectory = $initialDirectory
+$OpenFileDialog.filter = "All files (*.*)| *.*"
+$OpenFileDialog.ShowDialog() | Out-Null
+$OpenFileDialog.filename
+}
+
+
+
+Function Get-SaveFile($initialDirectory)
+{ 
+[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+Out-Null
+
+$SaveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+$SaveFileDialog.initialDirectory = $initialDirectory
+$SaveFileDialog.filter = "All files (*.*)| *.*"
+$SaveFileDialog.ShowDialog() | Out-Null
+$SaveFileDialog.filename
+} 
+
+
+# open dialog box to select the .nessuss file. 
+$InputFile = Get-OpenFile
+$OutputFile = Get-SaveFile
+
+
+#$Contents = [io.file]::ReadAllText($inputfile)
+#$Contents = [io.file]::ReadAllText('C:\tools\wd\nessus\data\data.xml')
+#$Global:OutFile = [System.IO.StreamWriter] "c:\tools\wd\nessus\outfile.csv"
+
+$InputFile
+$OutputFile
---
+++ Snipits/showhide.consle.ps1
@@ -0,0 +1,19 @@
+﻿Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+ 
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+ 
+function Show-Console {
+   $consolePtr = [Console.Window]::GetConsoleWindow()
+  #5 show
+ [Console.Window]::ShowWindow($consolePtr, 5)
+}
+ 
+function Hide-Console {
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+  #0 hide
+ [Console.Window]::ShowWindow($consolePtr, 0)
+}
